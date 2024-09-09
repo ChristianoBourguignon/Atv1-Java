d557c9ed96ca848606f193ddc55258d386a522cb
Christiano Bourguignon <christianombl@gmail.com>
2024-09-09 01:00:41 -0300

    feature: Adding a function for more optimized use

--- src/Main.java
+++ src/Main.java
@@ -13,44 +13,44 @@
 import java.util.Scanner;
 
 public class Main {
+
+    public static void GerarPiramide(int linha) {
+        // Criar a pirâmide
+        for (int i = 1; i <= linha; i++) {
+
+            // Adiconando espaço do lado esquerdo
+            for (int j = 1; j <= (linha - i); j++) {
+                System.out.print(" ");
+            }
+
+            // Primeira metade dos números
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+
+            // Segunda metade dos números
+            for (int j = i - 1; j >= 1; j--) {
+                System.out.print(j);
+            }
+            // Pular de linha
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
 
-        // Incluindo a função SCANNER
-        Scanner scanner = new Scanner(System.in);
+    // Incluindo a função SCANNER
+    Scanner scanner = new Scanner(System.in);
+    int nmrLinhas = 0;
+
+    while (nmrLinhas < 1 || nmrLinhas > 9){
 
         // Solicitando o número de linhas através do scanner
-        System.out.print("Digite o número desejado: " +
-                "(Entre 1 a 9)");
-        int nmrLinhas = scanner.nextInt();
+        System.out.print("Digite o número desejado: (Entre 1 a 9)");
 
-
-        if (nmrLinhas >= 1 && nmrLinhas < 9){
-
-            // Criar a pirâmide
-            for (int i = 1; i <= nmrLinhas; i++) {
-
-                // Adiconando espaço do lado esquerdo
-                for (int j = 1; j <= (nmrLinhas - i); j++) {
-                    System.out.print(" ");
-                }
-
-                // Primeira metade dos números
-                for (int j = 1; j <= i; j++) {
-                    System.out.print(j);
-                }
-
-                // Segunda metade dos números
-                for (int j = i - 1; j >= 1; j--) {
-                    System.out.print(j);
-                }
-                // Pular de linha
-                System.out.println();
-            }
-        } else {
-            System.out.println("O numero informado é " +
-                    "maior" +
-                    " que o 9 ou menor que 1.");
-        }
+        // Armazenando o número de linhas através do scanner
+        nmrLinhas = scanner.nextInt();
+    }
+    GerarPiramide(nmrLinhas);
     scanner.close();
     }
 }
