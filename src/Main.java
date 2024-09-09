e91432a7f701acff7577d35876073cb2e49f0f1a
Christiano Bourguignon <christianombl@gmail.com>
2024-09-09 00:28:56 -0300

    Primeiro commit pelo IntelliJ

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//2. [PIRAMIDE] Escreva um programa que mostre no console
+//uma pirâmide de números, com um número de linhas entre
+//1 e 9, fornecido pelo usuário. Caso o número esteja fora do
+//    limite, mostrar mensagem de erro e solicitar novamente
+//o número. Por exemplo, se o número de linhas for 4, o resultado
+//        será:
+//        1
+//        121
+//        12321
+//        1234321
+public class Main {
+    public static void main(String[] args) {
+        String mensagem = "Hellow World";
+        System.out.println(mensagem);
+    }
+}
